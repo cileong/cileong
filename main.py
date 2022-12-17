b0b9bfb5203fbac4e40b92fbf4c8f2343b7f7e4c
Ci Leong Ong <ongcileong2001@gmail.com>
2022-12-17 22:07:15 +0800

    Configure README to be self-updating

---
+++ main.py
@@ -0,0 +1,37 @@
+import chevron
+from datetime import datetime
+from pytz import timezone
+from pathlib import Path
+
+
+FNAME = "Ci Leong"
+LNAME = "Ong"
+
+POSITION = "Deep Learning Intern"
+COMPANY = "Aiology"
+
+TIMEZONE = timezone("Asia/Kuala_Lumpur")
+dt = datetime.now(TIMEZONE)
+STATUS = (
+    "Working 💻"
+    if dt.weekday() in range(1, 6) and dt.hour in range(9, 19)
+    else "Off work 💤"
+)
+
+
+DATA = {
+    "fname": FNAME.title(),
+    "lname": LNAME.upper(),
+    "position": POSITION.title(),
+    "company": COMPANY.title(),
+    "status": STATUS.capitalize(),
+}
+
+
+if __name__ == "__main__":
+
+    TEMPLATE_PATH = Path(__file__).parent / "template.mustache"
+    README_PATH = Path(__file__).parent / "README.md"
+
+    with TEMPLATE_PATH.open("r") as t, README_PATH.open("w") as r:
+        r.write(chevron.render(t, DATA))
